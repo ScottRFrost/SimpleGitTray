d8c8b6bc558faad81946bc26ecf2242cfc4aed2f
Scott R. Frost <ScottRFrost@gmail.com>
2018-04-23 09:34:41 -0400

    Initial commit

---
+++ SimpleGitTray/Program.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SimpleGitTray
+{
+    partial class form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form));
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuConfiguration = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "1 ↑  2 ↓";
+            this.trayIcon.Visible = true;
+            this.trayIcon.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trayIcon_MouseUp);
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuConfiguration,
+            this.menuExit,
+            this.toolStripSeparator1});
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(149, 54);
+            // 
+            // menuConfiguration
+            // 
+            this.menuConfiguration.Name = "menuConfiguration";
+            this.menuConfiguration.Size = new System.Drawing.Size(148, 22);
+            this.menuConfiguration.Text = "Configuration";
+            this.menuConfiguration.Click += new System.EventHandler(this.menuConfiguration_Click);
+            // 
+            // menuExit
+            // 
+            this.menuExit.Name = "menuExit";
+            this.menuExit.Size = new System.Drawing.Size(148, 22);
+            this.menuExit.Text = "Exit";
+            this.menuExit.Click += new System.EventHandler(this.menuExit_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(145, 6);
+            // 
+            // form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "form";
+            this.Text = "SimpleGitTray";
+            this.menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ContextMenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem menuConfiguration;
+        private System.Windows.Forms.ToolStripMenuItem menuExit;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+    }
+}
---
+++ SimpleGitTray/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibGit2Sharp;
+using Newtonsoft.Json;
+
+namespace SimpleGitTray
+{
+    public partial class form : Form
+    {
+        public Config config;
+
+        [STAThread]
+        public static void Main()
+        {
+            Application.Run(new form());
+        }
+
+        public form()
+        {
+            InitializeComponent();
+            config = JsonConvert.DeserializeObject<Config>(System.IO.File.ReadAllText("config.json"));
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false; // Hide form window.
+            ShowInTaskbar = false; // Remove from taskbar.
+
+            base.OnLoad(e);
+
+            trayIcon.BalloonTipTitle = "SimpleGitTray Loaded";
+            trayIcon.BalloonTipText = "Fetching from " + config.Repos.Length + " repos every " + config.FetchMinutes + " minutes";
+            trayIcon.ShowBalloonTip(1000);
+
+            Fetch();
+        }
+
+        public void Fetch()
+        {
+            // Clear Menu
+            while (menu.Items.Count > 3)
+            {
+                menu.Items.RemoveAt(3);
+            }
+
+            foreach (Repos r in config.Repos)
+            {
+                menu.Items.Add(new ToolStripLabel(r.Name));
+
+
+                using (var repo = new Repository(r.LocalPath))
+                {
+                    // Fetch
+                    //foreach (Remote remote in repo.Network.Remotes)
+                    //{
+                    //    IEnumerable<string> refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
+                    //    Commands.Fetch(repo, remote.Name, refSpecs, null, null);
+                    //}
+
+                    // Check for local changes
+                    var fileStatuses = new Dictionary<FileStatus, int>();
+                    foreach (StatusEntry statusEntry in repo.RetrieveStatus())
+                    {
+                        if (!fileStatuses.ContainsKey(statusEntry.State))
+                        {
+                            fileStatuses.Add(statusEntry.State, 1);
+                        }
+                        else
+                        {
+                            fileStatuses[statusEntry.State]++;
+                        }
+                    }
+                    if (fileStatuses.ContainsKey(FileStatus.Nonexistent))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.Nonexistent] + " Non-Existent"));
+                    if (fileStatuses.ContainsKey(FileStatus.NewInIndex))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.NewInIndex] + " New"));
+                    if (fileStatuses.ContainsKey(FileStatus.ModifiedInIndex))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.ModifiedInIndex] + " Modified"));
+                    if (fileStatuses.ContainsKey(FileStatus.DeletedFromIndex))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.DeletedFromIndex] + " Deleted"));
+                    if (fileStatuses.ContainsKey(FileStatus.RenamedInIndex))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.RenamedInIndex] + " Renamed"));
+                    if (fileStatuses.ContainsKey(FileStatus.TypeChangeInIndex))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.TypeChangeInIndex] + " Type Changed"));
+                    if (fileStatuses.ContainsKey(FileStatus.NewInWorkdir))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.NewInWorkdir] + " New (Uncommitted)"));
+                    if (fileStatuses.ContainsKey(FileStatus.ModifiedInWorkdir))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.ModifiedInWorkdir] + " Modified (Uncommitted)"));
+                    if (fileStatuses.ContainsKey(FileStatus.DeletedFromWorkdir))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.DeletedFromWorkdir] + " Deleted (Uncommitted)"));
+                    if (fileStatuses.ContainsKey(FileStatus.TypeChangeInWorkdir))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.TypeChangeInWorkdir] + " Type Changed (Uncommitted)"));
+                    if (fileStatuses.ContainsKey(FileStatus.RenamedInWorkdir))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.RenamedInWorkdir] + " Renamed (Uncommitted)"));
+                    if (fileStatuses.ContainsKey(FileStatus.Unreadable))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.Unreadable] + " Unreadable (In Working Dir)"));
+                    if (fileStatuses.ContainsKey(FileStatus.Conflicted))
+                        menu.Items.Add(new ToolStripLabel(fileStatuses[FileStatus.Conflicted] + " Conflicted"));
+
+                    // Check for diffs vs remote (might need to do them in reverse order too)
+                    var headTree = repo.Head.Tip.Tree;
+                    var remoteMasterTree = repo.Branches["origin/master"].Tip.Tree;
+                    var diffs = new Dictionary<ChangeKind, int>();
+                    foreach (TreeEntryChanges treeEntryChange in repo.Diff.Compare<TreeChanges>(remoteMasterTree, headTree))
+                    {
+                        if (!diffs.ContainsKey(treeEntryChange.Status))
+                        {
+                            diffs.Add(treeEntryChange.Status, 1);
+                        }
+                        else
+                        {
+                            diffs[treeEntryChange.Status]++;
+                        }
+                    }
+
+                    if (diffs.ContainsKey(ChangeKind.Added))
+                        menu.Items.Add(new ToolStripLabel(diffs[ChangeKind.Added] + " Added (Diff)"));
+                    if (diffs.ContainsKey(ChangeKind.Deleted))
+                        menu.Items.Add(new ToolStripLabel(diffs[ChangeKind.Deleted] + " Deleted (Diff)"));
+                    if (diffs.ContainsKey(ChangeKind.Modified))
+                        menu.Items.Add(new ToolStripLabel(diffs[ChangeKind.Modified] + " Modified (Diff)"));
+                    if (diffs.ContainsKey(ChangeKind.Renamed))
+                        menu.Items.Add(new ToolStripLabel(diffs[ChangeKind.Renamed] + " Renamed (Diff)"));
+                    if (diffs.ContainsKey(ChangeKind.Copied))
+                        menu.Items.Add(new ToolStripLabel(diffs[ChangeKind.Copied] + " Copied (Diff)"));
+                    if (diffs.ContainsKey(ChangeKind.Ignored))
+                        menu.Items.Add(new ToolStripLabel(diffs[ChangeKind.Ignored] + " Ignored (Diff)"));
+                    if (diffs.ContainsKey(ChangeKind.Untracked))
+                        menu.Items.Add(new ToolStripLabel(diffs[ChangeKind.Untracked] + " Untracked (Diff)"));
+                    if (diffs.ContainsKey(ChangeKind.TypeChanged))
+                        menu.Items.Add(new ToolStripLabel(diffs[ChangeKind.TypeChanged] + " TypeChanged (Diff)"));
+                    if (diffs.ContainsKey(ChangeKind.Unreadable))
+                        menu.Items.Add(new ToolStripLabel(diffs[ChangeKind.Unreadable] + " Unreadable (Diff)"));
+                    if (diffs.ContainsKey(ChangeKind.Conflicted))
+                        menu.Items.Add(new ToolStripLabel(diffs[ChangeKind.Conflicted] + " Conflicted (Diff)"));
+
+                }
+                menu.Items.Add(new ToolStripSeparator());
+            }
+            menu.Items.RemoveAt(menu.Items.Count - 1); // Remove trailing separator
+        }
+
+        #region "Menu"
+        private void trayIcon_MouseUp(object sender, MouseEventArgs e)
+        {
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    Visible = true;
+                    break;
+                case MouseButtons.Right:
+                    menu.Show();
+                    break;
+            }
+        }
+
+        private void menuConfiguration_Click(object sender, EventArgs e)
+        {
+            Process.Start("config.json");
+        }
+
+        private void menuExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        #endregion
+
+        #region "Config Structs"
+        public struct Repos
+        {
+            public string Name;
+            public string URL;
+            public string Branch;
+            public string Username;
+            public string Password;
+            public string LocalPath;
+        }
+
+        public struct Config
+        {
+            public int FetchMinutes;
+            public Repos[] Repos;
+        }
+        #endregion
+    }
+}

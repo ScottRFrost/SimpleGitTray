a7a2956312e8fda63707a21fd10ce53043de2cc9
Scott R. Frost <ScottRFrost@gmail.com>
2018-04-25 10:39:03 -0400

    Hard drive power died

--- SimpleGitTray/Program.cs
+++ SimpleGitTray/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using LibGit2Sharp;
+﻿using LibGit2Sharp;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace SimpleGitTray
 {
